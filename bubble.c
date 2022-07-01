2f20caa8ad92a3ffb046eaa99f5df4c5f4554428
abhijith-j <77925255+abhijith-j@users.noreply.github.com>
2022-07-01 16:06:40 +0530

    Bubble.c error
    line 7: j<5-k-1
    line 8: a[j]>a[j+1]
    line 10: a[j]=a[j+1]

--- bubble.c
+++ bubble.c
@@ -1,13 +1,13 @@
 //Question 5
-//Program to sort array using bubble sort
+//Program to sort array using bubble sort in ascending order
 #include<stdio.h>
 void main(){
     int a[5]={5,2,8,6,7};
     for(int k=0;k<5;k++){
-        for(int j=0;j<5-k-1;j++){
-            if(a[j]>a[j+1]){
+        for(int j=0;j<5-k;j++){
+            if(a[j]<a[j+1]){
                 int temp=a[j];
-                a[j]=a[j+1];
+                a[j+1]=a[j];
                 a[j+1]=temp;
             }
         }
