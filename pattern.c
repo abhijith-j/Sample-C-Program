d21237ebd9f4b47b99a093b089a445ae3d7ec010
abhijith-j <77925255+abhijith-j@users.noreply.github.com>
2022-07-01 20:20:28 +0530

    pattern error
    line 3: include<stdio.h>
    line 7: printf("*");
    line  6: j--

--- pattern.c
+++ pattern.c
@@ -1,10 +1,10 @@
 //Question 2
 //Program to print star pattern
-#include<stdio.h>
+#includ<stdio.h>
 void main(){
     for(int k=0;k<=5;k++){
-        for(int j=5-k;j>=0;j--){
-            printf("*");
+        for(int j=5-k;j>=0;j-){
+            printf(*);
         }
         printf("\n");
     }
