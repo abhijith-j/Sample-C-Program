77e95b4e600ec807f77e574cc843b61e0043173c
abhijith-j <77925255+abhijith-j@users.noreply.github.com>
2022-07-01 20:30:59 +0530

    patter2 error
    line 6: int k
    line 7:int j
    line 7: int j=0

--- pattern2.c
+++ pattern2.c
@@ -3,8 +3,8 @@
 #include<stdio.h>
 void main()
 {
-    for(int k=0;k<=5;k++){
-        for(int j=0;j<k;j++){
+    for(k=0;k<=5;k++){
+        for(j=k;j<k;j++){
             printf("*");
         }
         printf("\n");
