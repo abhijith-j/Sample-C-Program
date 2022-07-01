ab4c33e961f5cdaf84ee850427a82e740ec7e6b5
abhijith-j <77925255+abhijith-j@users.noreply.github.com>
2022-07-01 20:45:08 +0530

    selection error
    line 21: array[min]=array[i];
    line 9: min=i; should be inside first for loop
    line 8: {8,1,9,0,3};
    line 14: array[min]>array[j]

--- selection.c
+++ selection.c
@@ -5,20 +5,20 @@ void  main()
 {
     int i,j;
     int min;
-    int array[5]={8,1,9,0,3};
-    for(i=0;i<4;i++)
+    int array[5]=(8,1,9,0,3);
+    min=i;
+    for(i=0;i<5;i++)
     {
-        min=i;
         for(j=i+1;j<5;j++)
         {
-            if(array[min]>array[j])
+            if(array[min]<array[j])
             {
                 min=j;
             }
         }
 
         int temp=array[min];
-        array[min]=array[i];
+        array[i]=array[min];
         array[i]=temp;
     }
     for(i=0;i<5;i++)
