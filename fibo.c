349744c94951d18d23d58d1b7a55dac8927155ef
abhijith-j <77925255+abhijith-j@users.noreply.github.com>
2022-07-01 19:18:39 +0530

    fibo.c error
    line 9: k<n-2
    line 10: s= first+
    line 11: first= second

--- fibo.c
+++ fibo.c
@@ -6,9 +6,9 @@ void main()
     int first=0,second=1,s=0;
     int n=10;
     printf("%d %d",first,second);
-    for(int k=0;k<n-2;k++){
-        s= first+second;
-        first=second;
+    for(int k=0;k<n-1;k++){
+        s+= first+second;
+        second= first;
         second=s;
         printf(" %d",s);
     }
