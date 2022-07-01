6785909d941633baaf1b1171f7bc16d23504ff18
abhijith-j <77925255+abhijith-j@users.noreply.github.com>
2022-07-01 15:37:11 +0530

    Error programs.
    line 1: num=..
    line 14: sum+=
    line 17: sum==

--- armstrong.c
+++ armstrong.c
@@ -3,18 +3,18 @@
 //An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to the number itself.Eg.153 is an amstrong number.
 #include<stdio.h>
 #include<string.h>
-#include<math.h>
+#include<maths.h>
 void main()
 {
-    int num=154,numCopy=154;
+    int num=153,numCopy=154;
     int sum=0;
     while(num>0)
     {
         int rem=num%10;
-        sum+=rem*rem*rem;
+        sum=rem*rem*rem;
         num=num/10;
     }
-    if(sum==numCopy)
+    if(sum=numCopy)
         printf("%d is an armstrong number",numCopy);
     else
         printf("%d is not an armstrong number",numCopy);
